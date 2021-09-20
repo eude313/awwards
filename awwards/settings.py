78ea0bc565033b472ef93556153a640afa55ddca
eude313 <eudes.charles@student.moringaschool.com>
2021-09-20 21:45:04 +0300

    add modifications

--- awwards/settings.py
+++ awwards/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'DB_NAME': 'DB_NAME',
+        'DB_PASSWORD': 'DB_PASSWORD',
+        'DB_USER': 'DB_USER',
+        'DB_HOST': 'DB_HOST',
     }
 }
 
