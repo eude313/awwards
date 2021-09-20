8ff65e6ed922197ec1aae3a1578995ea9908c5ee
eude313 <eudes.charles@student.moringaschool.com>
2021-09-20 21:35:40 +0300

    add modifications

--- awward/views.py
+++ awward/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'awwards/home.html')
+
+def register(request):
+    return render(request, 'auth/register.html')
+
+def signIn(request):
+    return render(request, 'auth/signIn.html')
