d03f7633e811decbe6dd97221d4f109aea20de71
eude313 <eudes.charles@student.moringaschool.com>
2021-09-23 15:39:21 +0300

    add navbar

--- awward/views.py
+++ awward/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -6,16 +7,14 @@ def home(request):
     return render(request, 'awwards/home.html')
 
 def register(request):
+    form = UserCreationForm()
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
-        user = 
-        
-    else:
-        return render(request, 'auth/register.html')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('signIn')
+    context = {'form': form}
+    return render(request, 'auth/register.html', context)
 
 def signIn(request):
     if request.method == 'POST':
--- awwards/settings.py
+++ awwards/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os 
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s=crmy(c49_$j=++5!j%f%9j)-_6e!dy3s%zst4)3njve_4&$c'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -79,10 +78,10 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'DB_NAME': config('DB_NAME'),
-        'DB_PASSWORD': config('DB_PASSWORD'),
-        'DB_USER': config('DB_USER'),
-        'DB_HOST': config('DB_HOST'),
+        'NAME': config('DB_NAME'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'USER': config('DB_USER'),
+        'HOST': config('DB_HOST'),
     }
 }
 
@@ -119,6 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'awward.Users'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
